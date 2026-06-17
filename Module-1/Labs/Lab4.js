5ea1fb81595819700a6e1a43234929221116bd87
crortolf <crortolf@gmail.com>
2026-06-16 22:12:24 -0400

    Lab 4 completed

---
+++ Module-1/Labs/Lab4.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    console.log(a + b);
+    return a + b;
+}
+
+function sub(a, b) {
+    console.log(a - b);
+    return a - b;
+}
+
+function mult(a, b) {
+    console.log(a * b);
+    return a * b;
+}
+
+function div(a, b) {
+    console.log(a / b);
+    return a / b;
+}
+
+function sayHi(name) {
+    console.log("Hello "+name);
+}
