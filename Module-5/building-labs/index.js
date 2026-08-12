c2bfae1282aea05fe8185939f89da9d3f759492d
crortolf <crortolf@gmail.com>
2026-08-11 20:24:30 -0400

    M5L2

--- Module-5/building-labs/index.js
+++ Module-5/building-labs/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const testRoutes = require("./routes/myTestRoutes");
 
 app.use("/", express.static("public"));
+app.use("/mytest", testRoutes);
 
 app.listen(80, () => console.log("listening on 80"));
